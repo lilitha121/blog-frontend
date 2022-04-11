9d14b9d80d4dcfaf1a28fb32a2967dd5dc2eb8d6
lilitha121 <ngelelilitha18@gmail.com>
2022-04-11 16:32:18 +0200

    homepage update and profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,8 @@
 <template>
 <section
-   class="vh-100 bg-image space"
+   class="vh-100  space"
     
-    style="
-    "
+   
 >
   <div class="container">
     <div class="row ">
@@ -103,19 +102,9 @@ export default {
   position: relative;
   text-align: center;
 }
-.section-heading h6 {
-  color: #47bb8e;
-  font-family: "Barlow Condensed", sans-serif;
-  font-size: 17px;
-  font-weight: 300;
-  letter-spacing: 14px;
-  margin-bottom: 5px;
-  text-transform: uppercase;
-}
 
-h6 {
-  line-height: 1.4;
-}
+
+
 .section-heading h1 {
   font-family: "Barlow Condensed", sans-serif;
   font-size: 80px;
@@ -124,36 +113,13 @@ h6 {
   text-shadow: 0 8px rgb(0 0 0 / 5%);
   text-transform: uppercase;
 }
-.section-heading span {
-  color: transparent;
-  font-family: "Barlow Condensed", sans-serif;
-  font-size: 10vw;
-  font-weight: 700;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  -webkit-text-stroke: 1px rgba(0, 0, 0, 0.2);
-  opacity: 0.6;
-}
+
 
 h1 {
   font-weight: 700;
   line-height: 1.4;
   margin-bottom: 0;
 }
-
-.breadcrumb {
-  background-color: #c5cee4;
-}
-
-.breadcrumb a {
-  text-decoration: none;
-}
-
-/* .container{
-  max-width: 520px !important;
-} */
 
 p {
   margin: 0;
@@ -167,182 +133,52 @@ p {
 img {
   width: 100px;
   height: 100px;
-  /* border-radius: 50%; */
   object-fit: cover;
 }
 
-.fab.fa-twitter {
-  color: #8ab7f1;
-}
 
-.fab.fa-instagram {
-  color: #e1306c;
-}
-
-.fab.fa-facebook-f {
-  color: #5999ec;
-}
 
 body{
     margin-top:20px;  
 }
 
-.thumb img {
-  height: auto;
-  max-width: 100%;
-  vertical-align: middle;
-}
-
-.post-user img {
-  border: 3px solid grey;
-}
-
-.widget {
-  position: relative;
-  margin-bottom: 30px;
-  padding: 15px 20px;
-  background: #fff;
-  border-radius: 3px;
-}
-
-.widget-controls {
-  position: absolute;
-  z-index: 1;
-  top: 0;
-  right: 0;
-  padding: 14px;
-  font-size: 12px;
-}
-
-.widget-controls>a {
-  padding: 1px 4px;
-  border-radius: 4px;
-  color: rgba(0,0,0,0.4);
-  -webkit-transition: color 0.15s ease-in-out;
-  -o-transition: color 0.15s ease-in-out;
-  transition: color 0.15s ease-in-out;
-}
-
-.thumb {
-  position: relative;
-  display: inline-block;
-  text-align: center;
-  width: 48px;
-}
-
-.thumb img {
-  height: auto;
-  max-width: 100%;
-  vertical-align: middle;
-}
-
-.post-user img {
-  border: 3px solid grey;
-}
 
 .fs-mini {
   font-size: 13px;
-}
-
-.bg-danger, .bg-info {
-  -webkit-transition: background-color 0.15s ease-in-out;
-  -o-transition: background-color 0.15s ease-in-out;
-  transition: background-color 0.15s ease-in-out;
-}
-
-.bg-danger {
-  background-color: #dd5826;
-}
-
-.bg-info {
-  background-color: #39bbdb;;
 }
 
 .text-white {
   color: grey;
 }
 
- .widget-middle-overflow {
-  position: relative;
-  margin: 0 -20px;
-}
-
-.windget-padding-md {
-  padding: 15px 20px;
-}
-
-.mb-lg {
-  margin-bottom: 30px;
-}
-
-.mt-lg {
-  margin-top: 30px;
-}
-
-.fw-semi-bold {
-  font-weight: 600;
-}
-
-.tags {
-  padding-left: 0;
-  list-style: none;
-}
-
-.tags>li>a {
-  padding: 2px 8px;
-  font-size: 13px;
-  border-radius: 6px;
-  border: 1px solid white;
-  color: inherit;
-  text-decoration: none;
-}
-
-.fs-mini {
-  font-size: 13px;
-}
-
-.text-light {
-  opacity: .8;
-}
-
 .mt-sm {
   margin-top: 10px;
 }
 
-.bg-body-light {
-  background-color: #f6f6f6;
-}
-
-.widget > footer {
-  margin: 0 -20px -15px;
-  padding: 30px 20px;
-}
-
-.post-links {
-  margin-bottom: 0;
-  font-size: 12px;
-  padding-left: 0;
-}
-
-.post-links>li {
-  float: left;
-  list-style: none;
-  margin:8px;
-}
-
-.post-links>li>a {
-  text-decoration: none;
-  color: #999;
-}
 .card4 {
   border: 3px solid grey !important;
-  /* padding-bottom: 500px; */
-  /* background-color: #152733; */
+  display: grid;
   color: grey;
   min-width: 540px;
   -webkit-border-radius: 10px;
   -moz-border-radius: 10px;
   border-radius: 10px;
-  /* height: 40% !important; */
+  justify-content: center;
 }
+
+
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
+ .card4[data-v-ced23842] {
+    border: 3px solid grey !important;
+    padding-bottom: 500px;
+    justify-content: center;
+    color: grey;
+    width: 100%;
+    border-radius: 10px;
+    height: 40% !important;
+
+}
+}
+
 </style>
